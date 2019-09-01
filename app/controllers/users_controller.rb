045e6165766caf5a6a4edcd95e1e3623195da3f7
Doaa <doaaismaelm@gmail.com>
2019-09-01 20:12:41 +0200

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
+  end
+
+  def update
+    current_user.update(user_params)
+    redirect_to current_user
+  end
+end

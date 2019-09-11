80591861c97c5f7a1b66ea60e5f94cd4a02df1b6
Doaa <doaaismaelm@gmail.com>
2019-09-12 00:13:42 +0200

    add hashtags relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   #Relations
   belongs_to :user
   has_one_attached :image
+  has_many :post_hash_tags
   has_many :hash_tags, through: :post_hash_tags
 
   #Validation
@@ -11,4 +12,16 @@ class Post < ApplicationRecord
     errors.add(:image, "can't be blank") unless image.attached?
   end
 
+  def extract_name_hash_tags
+    description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+  end
+
+  after_commit :create_hash_tags, on: :create
+
+  def create_hash_tags
+    # create hash_tags of Post
+    extract_name_hash_tags.each do |hash_tag|
+      hash_tags.create :name => hash_tag
+    end
+  end
 end
